3c2edfdd0c3433c0b89422cf5eca061318ac9302
liuzezhou <457410043@qq.com>
2016-05-08 11:53:31 +0800

    代码格式调整

--- WebSpider/src/test/java/com/cqupt/spider/test/DateTest.java
+++ WebSpider/src/test/java/com/cqupt/spider/test/DateTest.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class DateTest {
 	public static void main(String[] args) {
-		long time = Long.parseLong("1462114838269");
+		long time = Long.parseLong("1462199735798");
 		Date date = new Date(time);
 		SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println(sd.format(date));
